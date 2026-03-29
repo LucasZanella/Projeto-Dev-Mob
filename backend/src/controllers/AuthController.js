2e51cf36bbef9d23bad7b9b253d25dd125dffb6d
Lucas Zanella <111504566+LucasZanella@users.noreply.github.com>
2026-03-29 17:00:59 -0300

    ajuste3

--- backend/src/controllers/AuthController.js
+++ backend/src/controllers/AuthController.js
@@ -5,12 +5,12 @@ const jwt = require('jsonwebtoken');
 class AuthController {
     async register(req, res) {
         try {
-            const { name, email, password } = req.body;
+            const { name, email, password} = req.body;
             if (await User.findOne({ email })) {
                 return res.status(400).json({ error: "Email já cadastrado" });
             }
             const hash = await bcrypt.hash(password, 10);
-            const user = await User.create({ name, email, password: hash , role});
+            const user = await User.create({ name, email, password: hash});
             res.status(201).json(user);
         } catch (err) {
             res.status(500).json({ error: err.message });
